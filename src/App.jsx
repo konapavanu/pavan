f1f335c36412230196b17058e406ab303decabb2
jaya surya <suryasarisa99.com>
2023-08-30 18:47:30 +0530

    pavan

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import pavan from "./pavan.jpg";
 import pavan1 from "./pavan1.jpg";
 import pavan2 from "./pavan2.jpg";
+import { ImFacebook2 } from "react-icons/im";
+import { BsWhatsapp, BsInstagram } from "react-icons/bs";
 import "./styles.scss";
 function App() {
   return (
@@ -9,25 +11,21 @@ function App() {
       <div className="App">
         <h1>Kona Pavan</h1>
         <img src={pavan} alt="x" />
-        <Link
-          href={"https://www.instagram.com/_pavan_kona_/"}
-          className={"insta"}
-        >
+        <Link url={Insta("_pavan_kona_/")} className={"insta"}>
+          <BsInstagram />
           Instagram
         </Link>
 
         <img src={pavan1} alt="x" />
-        <Link
-          url={"https://www.facebook.com/pavankumar.kona.5"}
-          className={"fb"}
-        >
+        <Link url={FB("pavankumar.kona.5")} className={"fb"}>
+          <ImFacebook2 />
           Facebook
         </Link>
 
         <img src={pavan2} alt="x" />
         <Link url={WA("9553443471")} className={"wa"}>
-          {" "}
-          Whatsapp{" "}
+          <BsWhatsapp />
+          Whatsapp
         </Link>
       </div>
     </>
@@ -36,6 +34,12 @@ function App() {
 
 function WA(num) {
   return `https://api.whatsapp.com/send/?phone=${num}&text&type=phone_number&app_absent=0`;
+}
+function FB(id) {
+  return `"https://www.facebook.com/${id}"`;
+}
+function Insta(id) {
+  return `https://www.instagram.com/${id}`;
 }
 
 function Link({ className, children, url }) {
